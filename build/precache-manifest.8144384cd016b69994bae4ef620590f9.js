3caadb98b8d02188621b614e74e9becb718748a3
koriwi <kilian2798@gmail.com>
2020-03-12 18:45:21 +0100

    github io

---
+++ build/precache-manifest.8144384cd016b69994bae4ef620590f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cee985d85638f79b278174bee28a34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b92785a438106890e0f7",
+    "url": "/static/js/2.1dd4addb.chunk.js"
+  },
+  {
+    "revision": "a7ab412812a262c5ef8e085a5fc77bcf",
+    "url": "/static/js/2.1dd4addb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dbc9720ffef7450496e",
+    "url": "/static/js/main.27c3c6ea.chunk.js"
+  },
+  {
+    "revision": "b6ea86dbdb9050e765f6",
+    "url": "/static/js/runtime-main.e4cadcdd.js"
+  }
+]);
