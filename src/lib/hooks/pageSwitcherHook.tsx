d19b1e414ceab4dceb291be54f1e729aac22ec02
Kilian Gosewisch <kilian2798@gmail.com>
2022-05-20 23:21:31 +0200

    fix: double serial requests

--- src/lib/hooks/pageSwitcherHook.tsx
+++ src/lib/hooks/pageSwitcherHook.tsx
@@ -28,43 +28,55 @@ const findCollectionPage = (configState: ConfigState, name: string) => {
   });
 };
 
+const run = async (configState: ConfigState, appState: AppState) => {
+  try {
+    const res = await fetch("http://localhost:8080/window");
+    if (!res.ok) return false;
+    let name = await res.text();
+    let page = findPage(configState, name);
+    // if no, look if we have a collection
+    const collection =
+      page !== -1 ? findCollectionPage(configState, name) : undefined;
+    if (page + 1 || collection) {
+      console.time("page")
+      const currentPageString = await appState.serialApi?.getCurrentPage();
+      console.timeEnd("page")
+      if (currentPageString === undefined) return;
+      const currentPageIndex = parseInt(currentPageString);
+      if (page + 1) {
+        if (currentPageIndex !== page) appState.serialApi?.setCurrentPage(page);
+      } else if (collection) {
+        const currentId = configState.pages.sorted[currentPageIndex];
+        if (!collection.pages.includes(currentId)) {
+          const firstCollectionPage = configState.pages.sorted.findIndex(
+            (pid) => pid === collection.pages[0]
+          );
+          appState.serialApi?.setCurrentPage(firstCollectionPage);
+        }
+      }
+    }
+  } catch {
+    console.log("error, companion probably not running")
+  }
+}
+
 export const usePageSwitcher = (props: {
   configState: ConfigState;
   appState: AppState;
 }) => {
   const { configState, appState } = props;
   useEffect(() => {
-
     if (!(navigator as any).serial) return () => {};
+    let running = false;
     const interval = setInterval(async () => {
-      try {
-        const res = await fetch("http://localhost:8080/window");
-        if (!res.ok) return false;
-        let name = await res.text();
-        let page = findPage(configState, name);
-        // if no, look if we have a collection
-        const collection =
-          page !== -1 ? findCollectionPage(configState, name) : undefined;
-        if (page + 1 || collection) {
-          const currentPageString = await appState.serialApi?.getCurrentPage();
-          if (currentPageString === undefined) return;
-          const currentPageIndex = parseInt(currentPageString);
-          if (page + 1) {
-            if (currentPageIndex !== page) appState.serialApi?.setCurrentPage(page);
-          } else if (collection) {
-            const currentId = configState.pages.sorted[currentPageIndex];
-            if (!collection.pages.includes(currentId)) {
-              const firstCollectionPage = configState.pages.sorted.findIndex(
-                (pid) => pid === collection.pages[0]
-              );
-              appState.serialApi?.setCurrentPage(firstCollectionPage);
-            }
-          }
-        }
-      } catch {
-        console.log("error, companion probably not running")
+      console.log(interval);
+      if(!running){
+        console.time("a");
+        running = true;
+        await run(configState, appState);
+        running = false;
+        console.timeEnd("a");
       }
-      
     }, 300);
     return () => {console.log("clearing interval", interval); clearInterval(interval)};
   }, [configState, appState.serialApi])
