a81fc14c616ecd9f8cb3fda78348fea728338c4e
Kilian Gosewisch <kilian2798@gmail.com>
2022-10-01 03:38:21 +0200

    feat: add label to slider on hover

--- src/lib/components/Slider.tsx
+++ src/lib/components/Slider.tsx
@@ -23,23 +23,34 @@ export const FDSlider: React.FC<{
   disabled = false,
 }) => (
   <CtrlDuo>
-    <input
-      className={c(
-        `rounded-md overflow-hidden appearance-none h-6 w-44 flex-shrink-0 p-1`,
-        disabled ? "bg-primary-100" : "bg-primary-500",
-        className
-      )}
-      type="range"
-      min={min}
-      max={max}
-      step={step}
-      value={value}
-      disabled={disabled}
-      onChange={onChange}
-      title={
-        disabled ? "This option only works with Text AND Image provided" : ""
-      }
-    />
+    <div className="relative group">
+      <input
+        className={c(
+          `rounded-md overflow-hidden appearance-none h-6 w-44 flex-shrink-0 p-1`,
+          disabled ? "bg-primary-100" : "bg-primary-500",
+          className
+        )}
+        type="range"
+        min={min}
+        max={max}
+        step={step}
+        value={value}
+        disabled={disabled}
+        onChange={onChange}
+        title={
+          disabled ? "This option only works with Text AND Image provided" : ""
+        }
+        alt={value.toString()}
+      />
+      <div className="absolute justify-center hidden group-hover:flex -top-1 right-full z-30">
+        <div className="flex items-center">
+          <div className="bg-gray-400 px-2 border-primary-600 border-2 rounded-md select-none text-lg">
+            {value}
+          </div>
+          <div className="fat-arrow-right z-50" />
+        </div>
+      </div>
+    </div>
     <Value>{value}</Value>
   </CtrlDuo>
 );
