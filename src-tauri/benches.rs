a38d851df5dec7473f6b3358fd1f7319acd0b299
Kilian Gosewisch <kilian2798@gmail.com>
2022-10-01 02:16:33 +0200

    fix: benches

--- src-tauri/benches.rs
+++ src-tauri/benches.rs
@@ -1,20 +1,14 @@
 extern crate criterion;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-use fd_lib::os::refresh_ports;
 use fd_lib::system::SystemInfo;
 
-use std::sync::{Arc, Mutex};
-
 use fd_lib::os::get_current_window;
-use fd_lib::{serial::FDSerial, state::FDState};
+use fd_lib::serial::FDSerial;
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("refresh serial ports", |b| {
-        let state = Arc::new(Mutex::new(FDState {
-            current_window: "".into(),
-            serial: FDSerial::new(),
-        }));
-        b.iter(|| refresh_ports(&state, 0, |_ports| ()));
+        let mut serial = FDSerial::new();
+        b.iter(|| serial.refresh_ports(0, |_ports| ()));
     });
     c.bench_function("get current window", |b| {
         b.iter(|| get_current_window(|path| Some(path.into())))
